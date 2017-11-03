380caeeeb01869d3e55029964dd743ba6e085714
tantianran <15915822634@139.com>
2017-11-03 16:12:31 +0800

    Add new features

---
+++ vc/src/powercli/psscript/dbTests.ps1
@@ -0,0 +1,9 @@
+﻿
+#新建数据库
+#new-psdrive -name db -psp SQLite -root "Data Source=C:\VMware\data.sqlite"
+
+#创建表，表名字：vc_ps_netlabel，字段：netlabel
+#new-item db:/vc_ps_netlabel -netlabel text
+
+#插入数据到表
+#new-item db:/vc_ps_netlabel -netlabel '4562'
---
+++ vc/src/powercli/psscript/get_port_group.ps1
@@ -0,0 +1,18 @@
+﻿C:\VMware\vc\src\powercli\psscript\conn_vc_server.ps1
+
+Import-Module SQLite
+new-psdrive -name db -psp SQLite -root "Data Source=C:\VMware\data.sqlite"
+
+
+# 获取网络标签数据
+$label = Get-VirtualPortGroup
+
+#数据去重
+$dt = $label.Name | Sort-Object -Unique
+
+#写入数据库
+foreach ($n in $dt)
+{
+    Write-Host "数据 网络标签：$n 入库"
+    new-item db:/vc_ps_netlabel -netlabel $n
+}
